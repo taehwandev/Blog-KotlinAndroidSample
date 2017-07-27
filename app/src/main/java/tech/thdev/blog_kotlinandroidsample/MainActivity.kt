ca2e8bb21a010a048ad8de1873a1ad395b4acf12
TaeHwan <taehwan@thdev.net>
2017-07-27 23:55:19 +0900

    Add MainActivity.

---
+++ app/src/main/java/tech/thdev/blog_kotlinandroidsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package tech.thdev.blog_kotlinandroidsample
+
+import android.os.Bundle
+import tech.thdev.base.view.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
